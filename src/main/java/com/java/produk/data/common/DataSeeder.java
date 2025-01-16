adebd0e454fd14cd431595d5907d9f1b307b54dc
ida.sufiana <ida.sufiana@consultant.ioh.co.id>
2025-01-16 13:33:08 +0700

    update DataSeeder

--- src/main/java/com/java/produk/data/common/DataSeeder.java
+++ src/main/java/com/java/produk/data/common/DataSeeder.java
@@ -1,12 +1,18 @@
 package com.java.produk.data.common;
 
+import com.java.produk.data.constant.Group;
 import com.java.produk.data.model.Product;
+import com.java.produk.data.model.Role;
 import com.java.produk.data.model.User;
 import com.java.produk.data.repository.ProductRepository;
+import com.java.produk.data.repository.RoleRepository;
 import com.java.produk.data.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class DataSeeder implements CommandLineRunner {
@@ -14,14 +20,28 @@ public class DataSeeder implements CommandLineRunner {
     private UserRepository userRepository;
 
     @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
     private ProductRepository produkRepository;
 
     @Override
     public void run(String... args) throws Exception {
 
+        Role roleAdm = new Role();
+        roleAdm.setRole(Group.ROLE_ADMIN);
+        roleAdm.setDescription("");
+        roleRepository.save(roleAdm);
+
+        Set<Role> roles = new HashSet<>();
+        roles.add(roleAdm);
+
         User admin = new User();
         admin.setUserName("admin");
         admin.setPassword("adminpass");
+        admin.setStatus(true);
+        admin.setRoles(roles);
+
         userRepository.save(admin);
 
         Product produk = new Product();
